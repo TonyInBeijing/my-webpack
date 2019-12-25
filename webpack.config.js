82326a6b8d4eede92e3798acd8eb98c6b7f83822
TonyInBeijing <yuehaowei1126@163.com>
2019-12-25 16:55:18 +0800

    postcss & MiniCssExtarctPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,12 @@
+// commonJS 引入path模块
 var path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// 判断当前环境是开发环境还是 部署环境，主要是 mode属性的设置值。
+const devMode = process.env.NODE_ENV !== 'production';
 
+// 导出整个webpack配置对象
 module.exports = {
+    // webpack 提供 mode 配置选项，配置 webpack 相应模式的内置优化。
     mode: 'development',
     entry: './src/index.js',
     output: {
@@ -10,9 +16,31 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.css/,
-                use: ['style-loader', 'css-loader']
+                test: /\.(sa|c|sc)ss$/,
+                // css 和 style 模块解析的依赖 style-loader 和 css-loader
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            ident: 'postcss',
+                            sourceMap: true,
+                            plugins: loader => [
+                                require('autoprefixer')({ browsers: ['> 0.15% in CN'] }) // 添加前缀
+                            ]
+                        }
+                    },
+                    'sass-loader'
+                ]
             }
         ]
-    }
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            // 设置最终输出的文件名
+            filename: devMode ? '[name].css' : '[name].[hash].css',
+            chunkFilename: devMode ? '[id].css' : '[id].[hash].css'
+        })
+    ]
 };
